dd772adbba9fbc531c55aad809de490a23fd6a21
SYED RIYAZ <syedriyaz@192.168.1.10>
2023-03-04 21:49:02 +0530

    add

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,20 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.state={
+			show:false
+		}
 	};
 
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
+				{
+        this.state.show ? <h1 id='para'>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</h1> : null
+      }
+      <button id='click' onClick={() => { this.setShow(!this.state.show) }}>button</button>
+    </div>
+	    	);
     }
 }
 
