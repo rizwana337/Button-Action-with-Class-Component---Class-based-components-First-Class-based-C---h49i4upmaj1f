e13069f08d930d713b2b06f8649ae4e7cd1aacf1
SYED RIYAZ <syedriyaz@192.168.1.10>
2023-03-04 22:28:42 +0530

    addNS

--- src/components/App.js
+++ src/components/App.js
@@ -8,19 +8,14 @@ class App extends Component {
 			show:false
 		}
 	};
-onIncrementClickHandler=()=> {
-        // console.log("on click handler called");
-        // console.log(this);
-        this.setState({show:!this.state.show});
-                };
-			//	{age:this.state.age+1}	
+
     render() {
     	return(
     		<div id="main">
 				{
         this.state.show ? <h1 id='para'>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</h1> : null
       }
-      <button id='click' onClick={this.onIncrementClickHandler}>button</button>
+      <button id='click' onClick={() => this.setState({show:!this.state.show}) }>button</button>
     </div>
 	    	);
     }
